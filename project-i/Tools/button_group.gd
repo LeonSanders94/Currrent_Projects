38a787018bdb6fbddbff32d25caf564c7809ee93
LeonSanders94 <tylerbonatesta@gmail.com>
2026-02-15 01:05:08 -0500

    commit_2_ID_group
    refined IDs to register to the grid without the use of calling nodes directly.
    Generates an ID for each node associated with the tiles

--- project-i/Tools/button_group.gd
+++ project-i/Tools/button_group.gd
@@ -3,11 +3,19 @@ extends CanvasGroup
 var grid_registry = []
 @onready var button: Node2D = $"."
 
+
+
+
 func _ready() -> void:
-	## Need to read buttons
-	#.instance.connect(button.button_ready)
-	print(grid_registry)
+	print("Grid Registry: ",grid_registry)
+
 
 func _on_child_entered_tree(node: Node) -> void:
 	#if node.has_signal("button_ready"):
-		grid_registry.append(node.get_children())
+	var tile_id = "Tile: "
+	tile_id += (str(node).substr(25,-1))
+	grid_registry.append(tile_id)
+
+func _button_press():
+	
+	print("pressed! on grid!")
--- project-i/scripts/button.gd
+++ project-i/scripts/button.gd
@@ -1,16 +1,14 @@
 extends AnimatedSprite2D
 
+
 var mouse_in:bool
-@onready var button_group: CanvasGroup = $"../../.."
-@onready var button: Node2D = $".."
-signal button_ready(type,code)
-func _ready():
-	var self_name = str(self)
-	emit_signal("Button",self_name.substr(25,-1)) 
+
+
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_released("select") and mouse_in == true:
 		print("Pressed!")
+		emit_signal("Button") 
 
 func _on_button_area_mouse_entered() -> void:
 	$".".frame = 1
