7605339235018f7b387f04907ea8134aeeedeb7b
LeonSanders94 <tylerbonatesta@gmail.com>
2026-02-14 22:00:46 -0500

    Grid_work_2.14
    Added all buttons as children to the grid
    made each button a ID
    made a signal from the the button when it enters the scene
    Grid registers all the nodes to an array

--- project-i/Tools/button_group.gd
+++ project-i/Tools/button_group.gd
@@ -1,27 +1,13 @@
 extends CanvasGroup
 
-@onready var button_group: CanvasGroup = $"."
-@export var button:PackedScene
-var grid_row = []
-var grid_column = []
-
+var grid_registry = []
+@onready var button: Node2D = $"."
 
 func _ready() -> void:
-	var a = button.instantiate()
+	## Need to read buttons
+	#.instance.connect(button.button_ready)
+	print(grid_registry)
 
-	self.position.x += 1
-	print("Button Group Pos: ", button_group.position,global_position.x)
-	spawn_grid_tile(a)
-
-	global_position.x += 1
-	print("Button Group Pos: ", button_group.position,global_position.x)
-	spawn_grid_tile(a)
-
-func spawn_grid_tile(Node):
-	add_child(Node)
-	print(get_children(),get_child_count())
-	print(get_child(0))
-	print(get_child(1))
-	print(get_child(0).position)
-	grid_row.append(str(Node))
-	print(grid_row)
+func _on_child_entered_tree(node: Node) -> void:
+	#if node.has_signal("button_ready"):
+		grid_registry.append(node.get_children())
